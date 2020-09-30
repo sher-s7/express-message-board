2c596919cc73448d67126c819da6c184f87f2980
Sher <sher.sheikh77@gmail.com>
2020-09-30 17:11:47 -0400

    Add new message functionality

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: new Date(),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
-  }
+    added: new Date(),
+  },
 ];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Messageboard', messages: messages });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+/* GET new message page. */
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
